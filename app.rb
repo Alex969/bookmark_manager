f5428715615a210c94264768de9750ff75316fb5
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-10-14 12:29:57 +0200

    add the add bookmark feature through a form

--- app.rb
+++ app.rb
@@ -16,4 +16,15 @@ class BookmarkManager < Sinatra::Base
     erb :'bookmarks/index'
   end
 
+  get '/bookmarks/new' do
+    erb :'bookmarks/new'
+  end
+
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
+  end
+
 end
--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -1,9 +1,9 @@
-feature 'adding a new bookmark' do
-  scenario 'A user can add a bookmark to the manager' do
+feature 'Adding a new bookmark' do
+  scenario 'A user can add a bookmark to Bookmark Manager' do
     visit('/bookmarks/new')
     fill_in('url', with: 'http://example.org')
     click_button('Submit')
 
-    expect(page).to have_content('http://example.org')
+    expect(page).to have_content 'http://example.org'
   end
-end 
+end
