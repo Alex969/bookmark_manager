262a67f5c63a67cd75250c28dcb230d7d53623b9
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-10-15 13:02:49 +0200

    completed step 11 but still 2 rspec tests not passing

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -7,15 +7,15 @@ feature 'Viewing bookmarks' do
   end
 
   scenario 'user can see the bookmarks' do
-    Bookmark.create(url: "http://www.google.com")
-    Bookmark.create(url: "http://www.amazon.com")
-    Bookmark.create(url: "http://www.twitter.com")
+    Bookmark.create(url: "http://www.google.com", title: "Google")
+    Bookmark.create(url: "http://www.amazon.com", title: "Amazon")
+    Bookmark.create(url: "http://www.twitter.com", title: "Twitter")
 
     visit('/bookmarks')
 
-    expect(page).to have_content "www.google.com"
-    expect(page).to have_content "www.amazon.com"
-    expect(page).to have_content "www.twitter.com"
+    expect(page).to have_link('Google', href: 'http://www.google.com')
+    expect(page).to have_link('Amazon', href: 'http://www.amazon.com')
+    expect(page).to have_link('Twitter', href: 'http://www.twitter.com')
 
   end
 end
